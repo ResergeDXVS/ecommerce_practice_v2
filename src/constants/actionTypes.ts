302ffbf59c73c04f36febfe0f2fb71d79ff38bf4
Sergio Reza <sergiogrezach@gmail.com>
2026-08-20 14:52:31 -0600

    [FIX] Ajustes para agregar API en las solicitudes con el backend

---
+++ src/constants/actionTypes.ts
@@ -0,0 +1,12 @@
+// Manejo de API
+// Consulta de reporte de información
+export const FETCH_REPORT = "report/fetchReport";
+// Crear registro de Guia de paquetería
+export const CREATE_GUIDES = "guides/createGuides";
+// Consultar lista de paquetes
+export const GET_GUIDES = "guides/getGuides";
+// Actualizar registros de paquetes
+export const PUT_GUIDES = "guides/putGuides";
+// Consultar historial de un paquete
+export const FETCH_HISTORY = "history/fetchHistory";
+
